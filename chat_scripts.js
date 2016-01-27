f25607bcaeb066c90a04488d924676fed5b7f5b3
maukka76 <markus.veijola@gmail.com>
2016-01-27 08:33:16 +0200

    socket example

---
+++ chat_scripts.js
@@ -0,0 +1,27 @@
+window.onload = function(event){
+	console.log('document is ready');
+	//Connect to server with socket
+	var socket = io();
+	
+	
+	//Listen "new message" from server
+	socket.on('new message',function(data){
+		//Get textarea with id="messages"
+		var textArea = document.getElementById('messages');
+		textArea.value += data.name + ":" + data.message + "\n";
+	});
+	//Get object that has id="send"
+	var btnSend = document.getElementById('send');
+	//Add click listener for it
+	btnSend.onclick = function(){
+		
+		var msg = document.getElementById('chat_message');
+		console.log(msg.value);
+		var dataToServer = {
+			name:'undefined',
+			message:msg.value
+		}
+		//Send message to server
+		socket.emit('chat msg',dataToServer);
+	}
+}
